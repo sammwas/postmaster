19cd13965634847366072cf6da57d1e6bff3179a
sammwas <smwangi@abnosoftwares.co.ke>
2021-01-09 19:33:07 +0300

    Add project files.

---
+++ PosMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PosMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
