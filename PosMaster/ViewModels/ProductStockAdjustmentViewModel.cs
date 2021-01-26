f5e48397938353d10081a170d04dcb9a4b4ddc3b
Eric Karuri <e.karuri@yahoo.com>
2021-01-26 10:14:06 +0300

    secure cookies and implement stock adjustment

---
+++ PosMaster/ViewModels/ProductStockAdjustmentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PosMaster.ViewModels
+{
+	public class ProductStockAdjustmentViewModel : BaseViewModel
+	{
+		[Required]
+		[Display(Name = "Product")]
+		public string ProductId { get; set; }
+		[Display(Name = "Final Quantity")]
+		public decimal QuantityTo { get; set; }
+	}
+}
