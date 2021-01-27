56cbef4475289568e258c9aee7d2d2040d61d5df
sammwas <samson.m.njuguna@gmail.com>
2021-01-27 23:12:04 +0300

    add point of sale view

--- PosMaster/Controllers/PointOfSaleController.cs
+++ PosMaster/Controllers/PointOfSaleController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PosMaster.Dal;
+using PosMaster.Dal.Interfaces;
+using PosMaster.Extensions;
+using PosMaster.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +12,22 @@ namespace PosMaster.Controllers
 {
     public class PointOfSaleController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductInterface _productInterface;
+        private readonly ICookiesService _cookieService;
+        public PointOfSaleController(IProductInterface productInterface, ICookiesService cookieService)
+        {
+            _productInterface = productInterface;
+            _cookieService = cookieService;
+        }
+        //public async Task<IActionResult> Index()
+        //{
+        //    var user = _cookieService.Read();
+        //    var result = await _productInterface.ByInstanceIdAsync(user.InstanceId);
+        //    if (result.Success)
+        //        TempData.SetData(AlertLevel.Warning, "Products", result.Message);
+        //    return View(result.Data);
+        //}
+        public IActionResult Index() 
         {
             return View();
         }
