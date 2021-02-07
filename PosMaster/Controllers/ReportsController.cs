82ef436e198793d2e1e35d21dbd67907e024c085
Eric Karuri <e.karuri@yahoo.com>
2021-02-07 21:18:00 +0300

    show difference for weekly and monthly report

--- PosMaster/Controllers/ReportsController.cs
+++ PosMaster/Controllers/ReportsController.cs
@@ -1,38 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PosMaster.Dal;
 using PosMaster.Dal.Interfaces;
 using PosMaster.Extensions;
 using PosMaster.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PosMaster.Controllers
 {
-    public class ReportsController : Controller
-    {
-        private readonly IReportingInterface _reportingInterface;
-        private readonly ICookiesService _cookiesService;
-        public ReportsController(IReportingInterface reportingInterface, ICookiesService cookiesService)
-        {
-            _reportingInterface = reportingInterface;
-            _cookiesService = cookiesService;
-        }
-        public async Task<IActionResult> SalesReport(string instanceId = "", string dtFrom = "", string dtTo = "", string search = "")
-        {
-            ViewData["dtTo"] = dtTo;
-            ViewData["dtFrom"] = dtFrom;
-            ViewData["instanceId"] = instanceId;
-            var userData = _cookiesService.Read();
-            Guid? clientId;
-            if (!User.IsInRole(Role.SuperAdmin))
-                clientId = userData.ClientId;
-            else clientId = null;
-            var result = await _reportingInterface.DailySalesReportAsync(clientId, instanceId, dtFrom, dtTo, search);
-            if (!result.Success)
-                TempData.SetData(AlertLevel.Warning, "Product Categories", result.Message);
-            return View(result.Data);
-        }
-    }
+	[Authorize]
+	public class ReportsController : Controller
+	{
+		private readonly IReportingInterface _reportingInterface;
+		private readonly ICookiesService _cookiesService;
+		public ReportsController(IReportingInterface reportingInterface, ICookiesService cookiesService)
+		{
+			_reportingInterface = reportingInterface;
+			_cookiesService = cookiesService;
+		}
+		public async Task<IActionResult> SalesReport(string instanceId = "", string dtFrom = "", string dtTo = "", string search = "", string option = "")
+		{
+			ViewData["dtTo"] = dtTo;
+			ViewData["dtFrom"] = dtFrom;
+			ViewData["instanceId"] = instanceId;
+			ViewData["option"] = option;
+			var userData = _cookiesService.Read();
+			Guid? clientId;
+			if (!User.IsInRole(Role.SuperAdmin))
+				clientId = userData.ClientId;
+			else clientId = null;
+			var result = await _reportingInterface.DailySalesReportAsync(clientId, instanceId, dtFrom, dtTo, search);
+			if (!result.Success)
+				TempData.SetData(AlertLevel.Warning, $"{option} Report", result.Message);
+			return View(result.Data);
+		}
+	}
 }
