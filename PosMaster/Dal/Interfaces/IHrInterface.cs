61064c8045220c217642f781b38068288032858e
Eric Karuri <e.karuri@yahoo.com>
2021-03-23 19:01:11 +0300

    implement employee salaries

--- PosMaster/Dal/Interfaces/IHrInterface.cs
+++ PosMaster/Dal/Interfaces/IHrInterface.cs
@@ -13,6 +13,12 @@ namespace PosMaster.Dal.Interfaces
 		Task<ReturnData<Bank>> EditBankAsync(BankViewModel model);
 		Task<ReturnData<Bank>> BankByIdAsync(Guid id);
 		Task<ReturnData<List<Bank>>> BanksAsync(Guid clientId);
+		Task<ReturnData<EmployeeLeaveCategory>> EditLeaveCategoryAsync(EmployeeLeaveCategoryViewModel model);
+		Task<ReturnData<EmployeeLeaveCategory>> LeaveCategoryByIdAsync(Guid id);
+		Task<ReturnData<List<EmployeeLeaveCategory>>> LeaveCategoriesAsync(Guid clientId);
+		Task<ReturnData<EmployeeSalary>> EditEmployeeSalaryAsync(EmployeeSalaryViewModel model);
+		Task<ReturnData<EmployeeSalary>> EmployeeSalaryByIdAsync(string userId);
+		Task<ReturnData<List<EmployeeSalary>>> EmployeeSalariesAsync(Guid clientId);
 	}
 
 	public class HrImplementation : IHrInterface
@@ -135,6 +141,251 @@ namespace PosMaster.Dal.Interfaces
 				result.Message = "Added";
 				result.Data = bank;
 				_logger.LogInformation($"{tag} added {bank.Name}  {bank.Id} : {result.Message}");
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				result.ErrorMessage = ex.Message;
+				result.Message = "Error occured";
+				_logger.LogError($"{tag} {result.Message} : {ex}");
+				return result;
+			}
+		}
+
+		public async Task<ReturnData<EmployeeSalary>> EditEmployeeSalaryAsync(EmployeeSalaryViewModel model)
+		{
+			var result = new ReturnData<EmployeeSalary> { Data = new EmployeeSalary() };
+			var tag = nameof(EditEmployeeSalaryAsync);
+			_logger.LogInformation($"{tag} edit employee salary for {model.UserId}");
+			try
+			{
+
+				var salary = await _context.EmployeeSalaries
+					.FirstOrDefaultAsync(c => c.UserId.Equals(model.UserId));
+				if (salary == null)
+				{
+					var empSalary = new EmployeeSalary
+					{
+						ClientId = model.ClientId,
+						InstanceId = model.InstanceId,
+						UserId = model.UserId,
+						Allowance = model.Allowance,
+						Deduction = model.Deduction,
+						BasicPay = model.BasicPay,
+						Bank = model.Bank,
+						BankAccount = model.BankAccount,
+						Personnel = model.Personnel,
+						Status = model.Status,
+						Notes = model.Notes
+					};
+					_context.EmployeeSalaries.Add(empSalary);
+					await _context.SaveChangesAsync();
+					result.Success = true;
+					result.Message = "Added";
+					result.Data = empSalary;
+					return result;
+				}
+				var prevNet = salary.NetAmount;
+				salary.Code = model.Code;
+				salary.Allowance = model.Allowance;
+				salary.BasicPay = model.BasicPay;
+				salary.Deduction = model.Deduction;
+				salary.Bank = model.Bank;
+				salary.BankAccount = model.BankAccount;
+				salary.LastModifiedBy = model.Personnel;
+				salary.DateLastModified = DateTime.Now;
+				salary.Notes = model.Notes;
+				salary.Status = model.Status;
+				if (prevNet != salary.NetAmount)
+				{
+					var salaryLog = new EmployeeSalaryLog
+					{
+						ClientId = model.ClientId,
+						InstanceId = model.InstanceId,
+						UserId = model.UserId,
+						Personnel = model.Personnel,
+						SalaryFrom = prevNet,
+						SalaryTo = salary.NetAmount
+					};
+					_context.EmployeeSalaryLogs.Add(salaryLog);
+				}
+				await _context.SaveChangesAsync();
+				result.Success = true;
+				result.Message = "Updated";
+				result.Data = salary;
+				_logger.LogInformation($"{tag} updated {salary.Code} {model.Id} : {result.Message}");
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				result.ErrorMessage = ex.Message;
+				result.Message = "Error occured";
+				_logger.LogError($"{tag} {result.Message} : {ex}");
+				return result;
+			}
+		}
+
+		public async Task<ReturnData<EmployeeLeaveCategory>> EditLeaveCategoryAsync(EmployeeLeaveCategoryViewModel model)
+		{
+			var result = new ReturnData<EmployeeLeaveCategory> { Data = new EmployeeLeaveCategory() };
+			var tag = nameof(EditLeaveCategoryAsync);
+			_logger.LogInformation($"{tag} edit leave category");
+			try
+			{
+				if (model.IsEditMode)
+				{
+					var dbCategory = await _context.EmployeeLeaveCategories
+						.FirstOrDefaultAsync(c => c.Id.Equals(model.Id));
+					if (dbCategory == null)
+					{
+						result.Message = "Not Found";
+						_logger.LogInformation($"{tag} update failed {model.Id} : {result.Message}");
+						return result;
+					}
+					dbCategory.Title = model.Title;
+					dbCategory.Code = model.Code;
+					dbCategory.MaxDays = model.MaxDays;
+					dbCategory.AllowedGender = model.FemaleOnly ? "Female" : "";
+					dbCategory.LastModifiedBy = model.Personnel;
+					dbCategory.DateLastModified = DateTime.Now;
+					dbCategory.Notes = model.Notes;
+					dbCategory.Status = model.Status;
+					await _context.SaveChangesAsync();
+					result.Success = true;
+					result.Message = "Updated";
+					result.Data = dbCategory;
+					_logger.LogInformation($"{tag} updated {dbCategory.Title} {model.Id} : {result.Message}");
+					return result;
+				}
+
+				var category = new EmployeeLeaveCategory
+				{
+					Title = model.Title,
+					Notes = model.Notes,
+					ClientId = model.ClientId,
+					Personnel = model.Personnel,
+					Status = model.Status,
+					Code = model.Code,
+					MaxDays = model.MaxDays,
+					AllowedGender = model.FemaleOnly ? "Female" : "",
+					InstanceId = model.InstanceId
+				};
+				_context.EmployeeLeaveCategories.Add(category);
+				await _context.SaveChangesAsync();
+				result.Success = true;
+				result.Message = "Added";
+				result.Data = category;
+				_logger.LogInformation($"{tag} added {category.Title}  {category.Id} : {result.Message}");
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				result.ErrorMessage = ex.Message;
+				result.Message = "Error occured";
+				_logger.LogError($"{tag} {result.Message} : {ex}");
+				return result;
+			}
+		}
+
+		public async Task<ReturnData<List<EmployeeSalary>>> EmployeeSalariesAsync(Guid clientId)
+		{
+			var result = new ReturnData<List<EmployeeSalary>> { Data = new List<EmployeeSalary>() };
+			var tag = nameof(EmployeeSalariesAsync);
+			_logger.LogInformation($"{tag} get client employee salaries");
+			try
+			{
+				var data = await _context.EmployeeSalaries.Include(s => s.User)
+					.Where(b => b.ClientId.Equals(clientId))
+					.OrderByDescending(c => c.DateCreated)
+					.ToListAsync();
+				result.Success = data.Any();
+				result.Message = result.Success ? "Found" : "Not Found";
+				if (result.Success)
+					result.Data = data;
+				_logger.LogInformation($"{tag} found {data.Count} employee salary records");
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				result.ErrorMessage = ex.Message;
+				result.Message = "Error occured";
+				_logger.LogError($"{tag} {result.Message} : {ex}");
+				return result;
+			}
+		}
+
+		public async Task<ReturnData<EmployeeSalary>> EmployeeSalaryByIdAsync(string userId)
+		{
+			var result = new ReturnData<EmployeeSalary> { Data = new EmployeeSalary() };
+			var tag = nameof(EmployeeSalaryByIdAsync);
+			_logger.LogInformation($"{tag} get employee {userId} salary");
+			try
+			{
+				var salary = await _context.EmployeeSalaries
+					.FirstOrDefaultAsync(c => c.UserId.Equals(userId));
+				result.Success = salary != null;
+				result.Message = result.Success ? "Found" : "Not Found";
+				if (result.Success)
+					result.Data = salary;
+				_logger.LogInformation($"{tag} salary record for {userId} {result.Message}");
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				result.ErrorMessage = ex.Message;
+				result.Message = "Error occured";
+				_logger.LogError($"{tag} {result.Message} : {ex}");
+				return result;
+			}
+		}
+
+		public async Task<ReturnData<List<EmployeeLeaveCategory>>> LeaveCategoriesAsync(Guid clientId)
+		{
+			var result = new ReturnData<List<EmployeeLeaveCategory>> { Data = new List<EmployeeLeaveCategory>() };
+			var tag = nameof(LeaveCategoriesAsync);
+			_logger.LogInformation($"{tag} get all client leave categories");
+			try
+			{
+				var data = await _context.EmployeeLeaveCategories
+					.Where(b => b.ClientId.Equals(clientId))
+					.OrderByDescending(c => c.DateCreated)
+					.ToListAsync();
+				result.Success = data.Any();
+				result.Message = result.Success ? "Found" : "Not Found";
+				if (result.Success)
+					result.Data = data;
+				_logger.LogInformation($"{tag} found {data.Count} client leave categories");
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				result.ErrorMessage = ex.Message;
+				result.Message = "Error occured";
+				_logger.LogError($"{tag} {result.Message} : {ex}");
+				return result;
+			}
+		}
+
+		public async Task<ReturnData<EmployeeLeaveCategory>> LeaveCategoryByIdAsync(Guid id)
+		{
+			var result = new ReturnData<EmployeeLeaveCategory> { Data = new EmployeeLeaveCategory() };
+			var tag = nameof(LeaveCategoryByIdAsync);
+			_logger.LogInformation($"{tag} get leave category by id - {id}");
+			try
+			{
+				var category = await _context.EmployeeLeaveCategories
+					.FirstOrDefaultAsync(c => c.Id.Equals(id));
+				result.Success = category != null;
+				result.Message = result.Success ? "Found" : "Not Found";
+				if (result.Success)
+					result.Data = category;
+				_logger.LogInformation($"{tag} leave category {id} {result.Message}");
 				return result;
 			}
 			catch (Exception ex)
