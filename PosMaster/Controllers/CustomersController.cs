0ef7172a96b58acfad61e577644151f0cb2c5926
Eric Karuri <e.karuri@yahoo.com>
2021-03-29 19:26:19 +0300

    manage banks

--- PosMaster/Controllers/CustomersController.cs
+++ PosMaster/Controllers/CustomersController.cs
@@ -12,67 +12,68 @@ using System.Threading.Tasks;
 
 namespace PosMaster.Controllers
 {
-	[Authorize]
-	public class CustomersController : Controller
-	{
-		private readonly ICookiesService _cookieService;
-		private readonly ICustomerInterface _customerInterface;
-		public CustomersController(ICookiesService cookiesService, ICustomerInterface customerInterface)
-		{
-			_cookieService = cookiesService;
-			_customerInterface = customerInterface;
-		}
-		public async Task<IActionResult> ByClientId(Guid clientId)
-		{
-			var result = await _customerInterface.ByClientIdAsync(clientId);
-			if (!result.Success)
-				TempData.SetData(AlertLevel.Warning, "Customers", result.Message);
-			return View(result.Data);
-		}
-		public async Task<IActionResult> Edit(Guid? id)
-		{
-			var userData = _cookieService.Read();
-			if (id == null)
-				return View(new CustomerViewModel { Status = EntityStatus.Active });
+    [Authorize]
+    public class CustomersController : Controller
+    {
+        private readonly ICookiesService _cookieService;
+        private readonly ICustomerInterface _customerInterface;
+        public CustomersController(ICookiesService cookiesService, ICustomerInterface customerInterface)
+        {
+            _cookieService = cookiesService;
+            _customerInterface = customerInterface;
+        }
+        public async Task<IActionResult> ByClientId(Guid clientId)
+        {
+            var result = await _customerInterface.ByClientIdAsync(clientId);
+            if (!result.Success)
+                TempData.SetData(AlertLevel.Warning, "Customers", result.Message);
+            return View(result.Data);
+        }
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            var userData = _cookieService.Read();
+            if (id == null)
+                return View(new CustomerViewModel { Status = EntityStatus.Active });
 
-			var result = await _customerInterface.ByIdAsync(id.Value);
-			if (!result.Success)
-			{
-				TempData.SetData(AlertLevel.Warning, "Customers", result.Message);
-				return RedirectToAction(nameof(ByClientId), new { userData.ClientId });
-			}
+            var result = await _customerInterface.ByIdAsync(id.Value);
+            if (!result.Success)
+            {
+                TempData.SetData(AlertLevel.Warning, "Customers", result.Message);
+                return RedirectToAction(nameof(ByClientId), new { userData.ClientId });
+            }
 
-			var model = new CustomerViewModel(result.Data);
-			return View(model);
-		}
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(CustomerViewModel model)
-		{
-			var userData = _cookieService.Read();
-			model.ClientId = userData.ClientId;
-			model.InstanceId = userData.InstanceId;
-			model.Personnel = User.Identity.Name;
-			var option = model.IsEditMode ? "Update" : "Add";
-			var title = $"{option} Customer";
-			if (!ModelState.IsValid)
-			{
-				var message = "Missing fields";
-				TempData.SetData(AlertLevel.Warning, title, message);
-				return View(model);
-			}
+            var model = new CustomerViewModel(result.Data);
+            return View(model);
+        }
 
-			var result = await _customerInterface.EditAsync(model);
-			TempData.SetData(result.Success ? AlertLevel.Success : AlertLevel.Warning, title, result.Message);
-			if (!result.Success)
-				return View(model);
-			return RedirectToAction(nameof(ByClientId), new { userData.ClientId });
-		}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CustomerViewModel model)
+        {
+            var userData = _cookieService.Read();
+            model.ClientId = userData.ClientId;
+            model.InstanceId = userData.InstanceId;
+            model.Personnel = User.Identity.Name;
+            var option = model.IsEditMode ? "Update" : "Add";
+            var title = $"{option} Customer";
+            if (!ModelState.IsValid)
+            {
+                var message = "Missing fields";
+                TempData.SetData(AlertLevel.Warning, title, message);
+                return View(model);
+            }
 
-		public async Task<JsonResult> Search(Guid cId, string term)
-		{
-			var data = await _customerInterface.SearchClientCustomerAsync(cId, term);
-			return Json(data);
-		}
-	}
+            var result = await _customerInterface.EditAsync(model);
+            TempData.SetData(result.Success ? AlertLevel.Success : AlertLevel.Warning, title, result.Message);
+            if (!result.Success)
+                return View(model);
+            return RedirectToAction(nameof(ByClientId), new { userData.ClientId });
+        }
+
+        public async Task<JsonResult> Search(Guid cId, string term)
+        {
+            var data = await _customerInterface.SearchClientCustomerAsync(cId, term);
+            return Json(data);
+        }
+    }
 }
---
+++ PosMaster/Controllers/HumanResourceController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PosMaster.Dal;
+using PosMaster.Dal.Interfaces;
+using PosMaster.Extensions;
+using PosMaster.Services;
+using PosMaster.ViewModels;
+using System;
+using System.Threading.Tasks;
+
+namespace PosMaster.Controllers
+{
+    [Authorize]
+    public class HumanResourceController : Controller
+    {
+        private readonly IHumanResourceInterface _humanResourceInterface;
+        private readonly ICookiesService _cookiesService;
+        public HumanResourceController(IHumanResourceInterface humanResourceInterface, ICookiesService cookiesService)
+        {
+            _humanResourceInterface = humanResourceInterface;
+            _cookiesService = cookiesService;
+        }
+
+        public async Task<IActionResult> EditBank(Guid? id)
+        {
+            var userData = _cookiesService.Read();
+            if (id == null)
+                return View(new BankViewModel { Status = EntityStatus.Active });
+
+            var result = await _humanResourceInterface.BankByIdAsync(id.Value);
+            if (!result.Success)
+            {
+                TempData.SetData(AlertLevel.Warning, "Banks", result.Message);
+                return RedirectToAction(nameof(ClientBanks), new { userData.ClientId });
+            }
+
+            var model = new BankViewModel(result.Data);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditBank(BankViewModel model)
+        {
+            var userData = _cookiesService.Read();
+            model.ClientId = userData.ClientId;
+            model.InstanceId = userData.InstanceId;
+            model.Personnel = User.Identity.Name;
+            var option = model.IsEditMode ? "Update" : "Add";
+            var title = $"{option} Bank";
+            if (!ModelState.IsValid)
+            {
+                var message = "Missing fields";
+                TempData.SetData(AlertLevel.Warning, title, message);
+                return View(model);
+            }
+
+            var result = await _humanResourceInterface.EditBankAsync(model);
+            TempData.SetData(result.Success ? AlertLevel.Success : AlertLevel.Warning, title, result.Message);
+            if (!result.Success)
+                return View(model);
+            return RedirectToAction(nameof(ClientBanks), new { userData.ClientId });
+        }
+
+        public async Task<IActionResult> ClientBanks(Guid clientId)
+        {
+            var result = await _humanResourceInterface.BanksAsync(clientId);
+            if (!result.Success)
+                TempData.SetData(AlertLevel.Warning, "Banks", result.Message);
+            return View(result.Data);
+        }
+    }
+}
