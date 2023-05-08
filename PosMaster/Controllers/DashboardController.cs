65e163071dbcd76bb911e14dcdf5a66c077daeb9
Eric Karuri <e.karuri@yahoo.com>
2023-05-08 17:42:34 +0300

    hide clerk dashboard

--- PosMaster/Controllers/DashboardController.cs
+++ PosMaster/Controllers/DashboardController.cs
@@ -9,63 +9,70 @@ using System.Threading.Tasks;
 
 namespace PosMaster.Controllers
 {
-	[Authorize]
-	public class DashboardController : Controller
-	{
-		private readonly ICookiesService _cookiesService;
-		private readonly IDashboardInterface _dashboardInterface;
-		private readonly string _tag = "Dashboard";
-		public DashboardController(ICookiesService cookiesService, IDashboardInterface dashboardInterface)
-		{
-			_cookiesService = cookiesService;
-			_dashboardInterface = dashboardInterface;
-		}
-		public IActionResult Index()
-		{
-			if (User.IsInRole(Role.SuperAdmin))
-				return RedirectToAction(nameof(SuperAdmin));
-			if (User.IsInRole(Role.Manager) || User.IsInRole(Role.Admin))
-				return RedirectToAction(nameof(Manager));
-			if (User.IsInRole(Role.Clerk))
-				return RedirectToAction(nameof(Clerk));
-			return RedirectToAction(nameof(Clerk));
-		}
-		public async Task<IActionResult> SuperAdmin()
-		{
-			var result = await _dashboardInterface.SuperAdminDashboardAsync();
-			if (!result.Success)
-				TempData.SetData(AlertLevel.Warning, _tag, result.Message);
-			var model = result.Success ? result.Data : new SuperAdminDashboardViewModel();
-			return View(model);
-		}
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly ICookiesService _cookiesService;
+        private readonly IClientInterface _clientInterface;
+        private readonly IDashboardInterface _dashboardInterface;
+        private readonly string _tag = "Dashboard";
+        public DashboardController(ICookiesService cookiesService, IDashboardInterface dashboardInterface,
+            IClientInterface clientInterface)
+        {
+            _cookiesService = cookiesService;
+            _dashboardInterface = dashboardInterface;
+            _clientInterface = clientInterface;
+        }
+        public async Task<IActionResult> Index()
+        {
+            if (User.IsInRole(Role.SuperAdmin))
+                return RedirectToAction(nameof(SuperAdmin));
+            if (User.IsInRole(Role.Manager) || User.IsInRole(Role.Admin))
+                return RedirectToAction(nameof(Manager));
+            if (User.IsInRole(Role.Clerk))
+            {
+                var clientRes = await _clientInterface.ByIdAsync(_cookiesService.Read().ClientId);
+                return clientRes.Data.DisplayBuyingPrice ?
+                 RedirectToAction(nameof(Clerk)) : RedirectToAction("Index", "PointOfSale");
+            }
+            return RedirectToAction(nameof(Clerk));
+        }
+        public async Task<IActionResult> SuperAdmin()
+        {
+            var result = await _dashboardInterface.SuperAdminDashboardAsync();
+            if (!result.Success)
+                TempData.SetData(AlertLevel.Warning, _tag, result.Message);
+            var model = result.Success ? result.Data : new SuperAdminDashboardViewModel();
+            return View(model);
+        }
 
-		public async Task<IActionResult> Manager()
-		{
-			var userData = _cookiesService.Read();
-			var result = await _dashboardInterface.ManagerDashboardAsync(userData.ClientId);
-			if (!result.Success)
-				TempData.SetData(AlertLevel.Warning, _tag, result.Message);
-			var model = result.Success ? result.Data : new ManagerDashboardViewModel();
-			return View(model);
-		}
-		public async Task<IActionResult> Clerk()
-		{
-			var userData = _cookiesService.Read();
-			var result = await _dashboardInterface.ClerkDashboardAsync(userData.InstanceId, User.Identity.Name);
-			if (!result.Success)
-				TempData.SetData(AlertLevel.Warning, _tag, result.Message);
-			var model = result.Success ? result.Data : new ClerkDashboardViewModel();
-			return View(model);
-		}
+        public async Task<IActionResult> Manager()
+        {
+            var userData = _cookiesService.Read();
+            var result = await _dashboardInterface.ManagerDashboardAsync(userData.ClientId);
+            if (!result.Success)
+                TempData.SetData(AlertLevel.Warning, _tag, result.Message);
+            var model = result.Success ? result.Data : new ManagerDashboardViewModel();
+            return View(model);
+        }
+        public async Task<IActionResult> Clerk()
+        {
+            var userData = _cookiesService.Read();
+            var result = await _dashboardInterface.ClerkDashboardAsync(userData.InstanceId, User.Identity.Name);
+            if (!result.Success)
+                TempData.SetData(AlertLevel.Warning, _tag, result.Message);
+            var model = result.Success ? result.Data : new ClerkDashboardViewModel();
+            return View(model);
+        }
 
-		public IActionResult Home()
-		{
-			return View();
-		}
+        public IActionResult Home()
+        {
+            return View();
+        }
 
-		public IActionResult Contact()
-		{
-			return View();
-		}
-	}
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
 }
--- PosMaster/ViewModels/MasterData/ClientViewModel.cs
+++ PosMaster/ViewModels/MasterData/ClientViewModel.cs
@@ -78,7 +78,7 @@ namespace PosMaster.ViewModels
         public int PhoneNumberLength { get; set; }
         [Display(Name = "Telephone Code")]
         public string TelephoneCode { get; set; }
-        [Display(Name = "Display Product Buying Price")]
+        [Display(Name = "Show Clerk Dashboard")]
         public bool DisplayBuyingPrice { get; set; }
         public bool IsNewImage { get; set; }
         public IFormFile File { get; set; }
